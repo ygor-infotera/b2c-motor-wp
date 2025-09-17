f5dcb11bf30030ed9ac7b0d4c6ea689219fc6b84
Ygor Perez <ygor@infotera.com.br>
2025-09-17 18:25:47 -0300

    Release v3.4: Convert advanced options to checkboxes
    - Updated whitelabel, iframe, and target to checkboxes for better UX
    - Improved form handling and JavaScript initialization
    - Updated documentation and version references
    - Version bump to 3.4

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Plugin Infotravel Motor para WordPress
 
-## Versão 3.3 - Motor Unificado
+## Versão 3.4 - Motor Unificado
 
 Este plugin WordPress fornece integração com os motores de busca do Infotravel para hotéis, voos, pacotes e serviços através de uma interface única e unificada.
 
 ## Download
 
-**Última Versão:** [v3.3](https://github.com/ygor-infotera/b2c-motor-wp/releases/tag/v3.3)
+**Última Versão:** [v3.4](https://github.com/ygor-infotera/b2c-motor-wp/releases/tag/v3.4)
 
-**Download Direto:** [Baixar v3.3](https://github.com/ygor-infotera/b2c-motor-wp/archive/refs/tags/v3.3.zip)
+**Download Direto:** [Baixar v3.4](https://github.com/ygor-infotera/b2c-motor-wp/archive/refs/tags/v3.4.zip)
 
 ## Funcionalidades
 
--- infotravel-motor.php
+++ infotravel-motor.php
@@ -11,7 +11,7 @@
      *  Plugin Name: Infotravel - Motor de Busca JS
      *  Plugin URI: https://github.com/InfoteraTecnologia/b2c-motor-wp
      *  Description: Plugin para inserção dos motores de busca do infotravel.
-     *  Version: 3.3
+     *  Version: 3.4
      *  Author: INFOTERA TECNOLOGIA
      *  Author URI: http://infotera.com.br/
      *  License:
@@ -19,7 +19,7 @@
      * Text Domain: infotravel-motor
      *
      * @author INFOTERA TECNOLOGIA
-     * @version 3.3
+     * @version 3.4
     */
 
 // Certifique-se de não expor nenhuma informação se for chamado diretamente
@@ -28,7 +28,7 @@ if (!function_exists('add_action')) {
     exit;
 }
 
-define('INFOTRAVEL_VERSION', '3.3');
+define('INFOTRAVEL_VERSION', '3.4');
 define('INFOTRAVEL__MINIMUM_WP_VERSION', '3.7');
 define('INFOTRAVEL__PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('INFOTRAVEL__PLUGIN_URL', plugins_url('', __FILE__));
